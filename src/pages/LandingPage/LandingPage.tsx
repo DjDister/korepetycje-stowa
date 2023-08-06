4745920b4c06cbc0db4ddb16f2a18d494b619b7a
Aleksy <psychologianauki@gmail.com>
2023-08-06 16:14:33 +0200

    branch init

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -5,7 +5,7 @@ import styles from "./LandingPage.module.css"
 export default function LandingPage() {
   return (
     <Layout>
-      <div className={styles.container}></div>
+      <div className={styles.container}>123</div>
     </Layout>
   )
 }
